2987654559aaa5caf6dc78e1e6741b821c8561fc
Rohit Gohri <rohit.gohri@smartprix.com>
2019-01-27 13:50:03 +0530

    move out restauarant logic to fn, handle some cases

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -1,9 +1,11 @@
 import Telegraf from 'telegraf';
+import d from 'sm-utils/d';
 
 import {
 	getLocation,
 	getTopRestaurants,
 	getNearbyRestaurantsAndLocation,
+	searchRestaurants,
 	convertToFloat
 } from '.';
 
@@ -16,6 +18,34 @@ async function init() {
 	bot.startPolling();
 }
 
+async function getRestaurants(query) {
+	let searchQuery = _.trim(query.query).toLowerCase();
+	let location;
+
+	const isNearQuery = searchQuery.includes('near ');
+	const nearSearchTerm = isNearQuery ? searchQuery.slice(searchQuery.indexOf('near ') + 5) : '';
+	d(isNearQuery, nearSearchTerm, searchQuery);
+
+	if (isNearQuery && nearSearchTerm) {
+		location = await getLocation(nearSearchTerm, query.location);
+
+		// Query starts with 'near' only then return top restaurants near that place
+		if (searchQuery.indexOf('near ') === 0 && location) {
+			return getTopRestaurants(location);
+		}
+
+		searchQuery = searchQuery.slice(0, searchQuery.indexOf('near '));
+	}
+	if (query.location && !location) {
+		const nearby = await getNearbyRestaurantsAndLocation(query.location.latitude, query.location.longitude);
+		location = nearby.location;
+
+		// If no search query return nearby restaurants
+		if(!searchQuery) return nearby.restaurants;
+	}
+	return searchRestaurants(searchQuery, location);
+}
+
 // error handler
 bot.use(async (ctx, next) => {
 	const start = Oak.time();
@@ -24,7 +54,7 @@ bot.use(async (ctx, next) => {
 		await next();
 	}
 	catch(err) {
-		Oak.error('Response error', err);
+		Oak.error(ctx.inlineQuery, 'Response error', err);
 	}
 	Oak.timeEnd(start, 'Response Time');
 });
@@ -32,53 +62,19 @@ bot.use(async (ctx, next) => {
 bot.on('inline_query', async (ctx) => {
 	if (!ctx.update.inline_query) return;
 	const query = ctx.update.inline_query;
-	const searchQuery = _.trim(query.query).toLowerCase();
-	let location;
-	let result: any = [];
-
-	const isNearQuery = searchQuery.includes('near ');
-	const nearSearchTerm = isNearQuery ? searchQuery.slice(searchQuery.indexOf('near ') + 5) : '';
-
-	if (isNearQuery && nearSearchTerm) {
-		location = await getLocation(nearSearchTerm, query.location);
-
-		// Query starts with 'near' only then return top restaurants near that place
-		if (searchQuery.indexOf('near ') === 0) {
-			const top = await getTopRestaurants(location);
-			result = top.map((r) => {
-				const restaurant = r.restaurant;
-				return {
-					type: 'venue',
-					id: restaurant.id,
-					latitude: convertToFloat(restaurant.location.latitude),
-					longitude: convertToFloat(restaurant.location.longitude),
-					title: restaurant.name,
-					address: `${restaurant.location.address}, ${restaurant.location.city}`,
-					thumb_url: restaurant.thumb,
-				};
-			});
-		}
-	}
-	else if (query.location) {
-		const nearby = await getNearbyRestaurantsAndLocation(query.location.latitude, query.location.longitude);
-		location = nearby.location;
-
-		// If no search query return nearby restaurants
-		if(!searchQuery) {
-			result = nearby.restaurants.map((r) => {
-				const restaurant = r.restaurant;
-				return {
-					type: 'venue',
-					id: restaurant.id,
-					latitude: convertToFloat(restaurant.location.latitude),
-					longitude: convertToFloat(restaurant.location.longitude),
-					title: restaurant.name,
-					address: `${restaurant.location.address}, ${restaurant.location.city}`,
-					thumb_url: restaurant.thumb,
-				};
-			});
-		}
-	}
+	const restaurants = await getRestaurants(query);
+	const result = restaurants.map((r) => {
+		const restaurant = r.restaurant;
+		return {
+			type: 'venue',
+			id: restaurant.id,
+			latitude: convertToFloat(restaurant.location.latitude),
+			longitude: convertToFloat(restaurant.location.longitude),
+			title: restaurant.name,
+			address: restaurant.location.address,
+			thumb_url: restaurant.thumb,
+		};
+	});
 
 	await ctx.answerInlineQuery(result);
 });
